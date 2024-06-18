0737b7c84088a154a0d5de14e1bc1164605c868d
apple <apple@apples-MacBook-Pro-2.local>
2024-06-18 23:32:20 +0530

    drinks.jsx add

--- frontend/src/components/Drinks.jsx
+++ frontend/src/components/Drinks.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import DrinkData from "../Data/Catagory.json"
+import CategoryIcon from "./CategoryIcon";
+import { PrimaryHeading, SecondaryHeading } from "./Heading";
 
 function Drinks() {
+  const DrinkCatagory = DrinkData.menu.find(catagory =>catagory.id===3)
   return (
-    <div>Drinks</div>
+    <div className=" flex justify-center items-center flex-col mt-8">
+      <PrimaryHeading>Side Food</PrimaryHeading>
+      <div className="flex flex-col">
+        {DrinkCatagory.items.map((type, index) => {
+          return (
+            <div  key={index} className="flex flex-col md:flex-row justify-start items-center md:gap-16 gap-8 bg-base md:m-10 m-3 p-4 text-wrap rounded-2xl">
+              <CategoryIcon
+               
+               
+                imgUrl={`/${type.image}`}
+              />
+              <div className="flex flex-col gap-2">
+                <SecondaryHeading>{type.name}</SecondaryHeading>
+                <p className="text-wrap"><span className="text-xl text-orange-300">Description:- </span>{type.description}</p>
+                <p className="text-wrap"> <span className="text-xl text-orange-300">Price:-  </span>$ {type.price}</p>
+                <p className="text-wrap"> <span className="text-xl text-orange-300">Sizes:- </span>{type.sizes}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    
+    </div>
   )
 }
 
