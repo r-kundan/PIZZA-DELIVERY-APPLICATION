f682773d08dd01a17fcb46862014cdb91710a52d
r-kundan <renkundan@gmail.com>
2024-07-06 21:52:40 +0530

    add filter functionality

--- frontend/src/components/Pizzas.jsx
+++ frontend/src/components/Pizzas.jsx
@@ -6,6 +6,7 @@ import RoundedButton from "./RoundedButton";
 
 import {useDispatch } from "react-redux" 
 import { addItem } from "../Redux/Slices/cartSlice";
+import { Link } from "react-router-dom";
 
 
 function Pizzas() {
@@ -26,6 +27,58 @@ function Pizzas() {
 
   return (
     <div className=" flex justify-center items-center flex-col mt-8">
+        <div className="flex justify-center items-center">
+      <ul className="flex md:gap-4 text-xl shadow-md m-2  mt-6 rounded-md">
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/pizzas"
+          >
+            Pizza
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/sides-food"
+          >
+            sidesFood
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/drinks"
+          >
+            Drinks
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/desserts"
+          >
+            Desserts
+          </Link>
+        </li>
+      </ul>
+      </div>
       <PrimaryHeading>Pizza Types</PrimaryHeading>
       <div className="flex flex-col">
         {PizzaData.pizzas.map((type, index) => {
--- frontend/src/components/SideFood.jsx
+++ frontend/src/components/SideFood.jsx
@@ -5,6 +5,7 @@ import { PrimaryHeading, SecondaryHeading } from "./Heading";
 import RoundedButton from "./RoundedButton";
 import {useDispatch } from "react-redux" 
 import { addItem } from "../Redux/Slices/cartSlice";
+import { Link } from "react-router-dom";
 
 function SideFood() {
   const dispatch = useDispatch()
@@ -22,6 +23,58 @@ function SideFood() {
   const sidesCategory = SidesFood.menu.find(category => category.id === 2);
   return (
     <div className=" flex justify-center items-center flex-col mt-8">
+         <div className="flex justify-center items-center">
+      <ul className="flex md:gap-4 text-xl shadow-md m-2  mt-6 rounded-md">
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/pizzas"
+          >
+            Pizza
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/sides-food"
+          >
+            sidesFood
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/drinks"
+          >
+            Drinks
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/desserts"
+          >
+            Desserts
+          </Link>
+        </li>
+      </ul>
+      </div>
       <PrimaryHeading>Side Food</PrimaryHeading>
       <div className="flex flex-col">
         {sidesCategory.items.map((type, index) => {
