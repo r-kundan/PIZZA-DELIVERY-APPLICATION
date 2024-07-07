a90632f2ade027881a00f889c7dac1489858de6e
r-kundan <renkundan@gmail.com>
2024-07-07 12:45:40 +0530

    filter in dessert

--- frontend/src/Pages/Menu.jsx
+++ frontend/src/Pages/Menu.jsx
@@ -8,58 +8,7 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <div className=" flex flex-col justify-center items-center">
-      <div className="flex justify-center items-center">
-      <ul className="flex md:gap-4 text-xl shadow-md m-2  mt-6 rounded-md">
-        <li className="m-2">
-          <Link
-            className={({ isActive }) =>
-              ` ${
-                isActive ? `text-primary` : "text-white "
-              } hover:underline hover:underline-offset-4`
-            }
-            to="/pizzas"
-          >
-            Pizza
-          </Link>
-        </li>
-        <li className="m-2">
-          <Link
-            className={({ isActive }) =>
-              ` ${
-                isActive ? `text-primary` : "text-white "
-              } hover:underline hover:underline-offset-4`
-            }
-            to="/sides-food"
-          >
-            sidesFood
-          </Link>
-        </li>
-        <li className="m-2">
-          <Link
-            className={({ isActive }) =>
-              ` ${
-                isActive ? `text-primary` : "text-white "
-              } hover:underline hover:underline-offset-4`
-            }
-            to="/drinks"
-          >
-            Drinks
-          </Link>
-        </li>
-        <li className="m-2">
-          <Link
-            className={({ isActive }) =>
-              ` ${
-                isActive ? `text-primary` : "text-white "
-              } hover:underline hover:underline-offset-4`
-            }
-            to="/desserts"
-          >
-            Desserts
-          </Link>
-        </li>
-      </ul>
-      </div>
+   
      
       <Desserts />
       <Pizzas />
--- frontend/src/components/Desserts.jsx
+++ frontend/src/components/Desserts.jsx
@@ -5,6 +5,7 @@ import { PrimaryHeading, SecondaryHeading } from "./Heading";
 import RoundedButton from "./RoundedButton";
 import {useDispatch } from "react-redux" 
 import { addItem } from "../Redux/Slices/cartSlice";
+import { Link } from "react-router-dom";
 
 
 
@@ -24,6 +25,58 @@ function Desserts() {
   const DessertCategory = DessertsData.menu.find(category => category.id === 4);
   return (
     <div className=" flex justify-center items-center flex-col mt-8">
+         <div className="flex justify-center items-center">
+      <ul className="flex md:gap-4 text-xl shadow-md m-2  mt-6 rounded-md">
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/pizzas"
+          >
+            Pizza
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/sides-food"
+          >
+            sidesFood
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/drinks"
+          >
+            Drinks
+          </Link>
+        </li>
+        <li className="m-2">
+          <Link
+            className={({ isActive }) =>
+              ` ${
+                isActive ? `text-primary` : "text-white "
+              } hover:underline hover:underline-offset-4`
+            }
+            to="/desserts"
+          >
+            Desserts
+          </Link>
+        </li>
+      </ul>
+      </div>
       <PrimaryHeading>Dessert Food</PrimaryHeading>
       <div className="flex flex-col">
         {DessertCategory.items.map((type, index) => {
